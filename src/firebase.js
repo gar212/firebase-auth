612e6d876b93b36c51c9aa3fa4bddf626a5f514f
gar212 <gartoyou@gmail.com>
2022-01-03 22:14:53 +1300

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBhhGB6eC5Hbbxrkbu07zB_Rsc3IPcOcVk",
+    authDomain: "auth-prac-a9e20.firebaseapp.com",
+    projectId: "auth-prac-a9e20",
+    storageBucket: "auth-prac-a9e20.appspot.com",
+    messagingSenderId: "332550215409",
+    appId: "1:332550215409:web:fb15b768633ed4ceb97b50"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
