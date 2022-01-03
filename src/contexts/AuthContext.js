5d03edf64bf85e824b8107173003b518091f26b6
gar212 <gartoyou@gmail.com>
2022-01-03 22:55:31 +1300

    .

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -1,6 +1,12 @@
 import React from 'react'
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 
 export default function AuthContext() {
+
+    const signup = (auth, email, password) => {
+        return createUserWithEmailAndPassword(auth, email, password)
+    }
+
     return (
         <div>
 
