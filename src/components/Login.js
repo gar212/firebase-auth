f0362e0f4e1af1be1dd23f2bcb3ff1496d053233
gar212 <gartoyou@gmail.com>
2022-01-09 13:55:44 +1300

    Add button variant

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,4 +1,4 @@
-import React, {useState, useRef} from 'react'
+import React, { useState, useRef } from 'react'
 import { Link, useNavigate } from 'react-router-dom';
 import { useAuth } from '../contexts/AuthContext';
 import Button from '@mui/material/Button';
@@ -12,32 +12,32 @@ export default function Login() {
 	const navigate = useNavigate();
 
 
-    const handleSubmit = async (e) => {
-			e.preventDefault();
-			try {
-				setStatus('');
-				setLoading(true);
-				await login(emailRef.current.value, passwordRef.current.value)
-				navigate('/');
-			} catch (e) {
-				setStatus('Incorrect email or password');
-				setLoading(false);
-			}
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		try {
+			setStatus('');
+			setLoading(true);
+			await login(emailRef.current.value, passwordRef.current.value)
+			navigate('/');
+		} catch (e) {
+			setStatus('Incorrect email or password');
+			setLoading(false);
+		}
 	}
 
-    return (
-        <div>
-            <h2>Log In</h2>
-				{status && <div className='error-message'>{status}</div>}
-            <form onSubmit={handleSubmit}>
-                <label htmlFor="email">Email Address</label>
-                <input type="email" ref={emailRef} />
-                <label htmlFor="password">Password</label>
-                <input type="password" ref={passwordRef} />
-                <Button disabled={loading} type='submit'>Log In</Button>
-            </form>
-						<p>Don't have an account? <Link to="/signup">Sign up</Link></p>
-						<Link to="/">Go back</Link>
-        </div>
-    )
+	return (
+		<div>
+			<h2>Log In</h2>
+			{status && <div className='error-message'>{status}</div>}
+			<form onSubmit={handleSubmit}>
+				<label htmlFor="email">Email Address</label>
+				<input type="email" ref={emailRef} />
+				<label htmlFor="password">Password</label>
+				<input type="password" ref={passwordRef} />
+				<Button variant="contained" disabled={loading} type='submit'>Log In</Button>
+			</form>
+			<p>Don't have an account? <Link to="/signup">Sign up</Link></p>
+			<Link to="/">Go back</Link>
+		</div>
+	)
 }
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,54 +1,54 @@
-import React, {useState, useRef} from 'react';
-import { useAuth,  } from '../contexts/AuthContext';
+import React, { useState, useRef } from 'react';
+import { useAuth, } from '../contexts/AuthContext';
 import Button from '@mui/material/Button';
-import {Link, useNavigate} from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 export default function Signup() {
-    const [status, setStatus] = useState('');
-    const [loading, setLoading] = useState(false);
-    const emailRef = useRef();
-    const passwordRef = useRef();
-		const passwordConfRef = useRef();
-    const { signup } = useAuth();
-    const navigate = useNavigate();
+  const [status, setStatus] = useState('');
+  const [loading, setLoading] = useState(false);
+  const emailRef = useRef();
+  const passwordRef = useRef();
+  const passwordConfRef = useRef();
+  const { signup } = useAuth();
+  const navigate = useNavigate();
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-				if (passwordConfRef.current.value !== passwordRef.current.value) {
-					return setStatus('Your passwords do not match');
-				}
-        try {
-					setStatus('');
-					setLoading(true);
-					await signup(emailRef.current.value, passwordRef.current.value)
-          navigate('/');
-        } catch (e) {
-            switch(e.message){
-                case "Firebase: Error (auth/email-already-in-use).":
-									setStatus('Email is already registered');
-									break;
-                default:
-                	setStatus('Failed to sign up');
-                  break;
-              }
-						setLoading(false);
-        }
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (passwordConfRef.current.value !== passwordRef.current.value) {
+      return setStatus('Your passwords do not match');
     }
-    
-    return (
-        <div>
-            <h2>Sign up</h2>
-						{status && <div className='error-message'>{status}</div>}
-            <form onSubmit={handleSubmit}>
-                <label htmlFor="email">Email Address</label>
-                <input type="email" ref={emailRef} />
-                <label htmlFor="password">Password</label>
-                <input type="password" ref={passwordRef} />
-                <label htmlFor="password">Password Confirmation</label>
-                <input type="password" ref={passwordConfRef} />
-                <Button disabled={loading} type='submit'>Sign up</Button>
-            </form>
-            <p>Don't have an account? <Link to="/login">Go Back</Link></p>
-        </div>
-    )
+    try {
+      setStatus('');
+      setLoading(true);
+      await signup(emailRef.current.value, passwordRef.current.value)
+      navigate('/');
+    } catch (e) {
+      switch (e.message) {
+        case "Firebase: Error (auth/email-already-in-use).":
+          setStatus('Email is already registered');
+          break;
+        default:
+          setStatus('Failed to sign up');
+          break;
+      }
+      setLoading(false);
+    }
+  }
+
+  return (
+    <div>
+      <h2>Sign up</h2>
+      {status && <div className='error-message'>{status}</div>}
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="email">Email Address</label>
+        <input type="email" ref={emailRef} />
+        <label htmlFor="password">Password</label>
+        <input type="password" ref={passwordRef} />
+        <label htmlFor="password">Password Confirmation</label>
+        <input type="password" ref={passwordConfRef} />
+        <Button variant="contained" disabled={loading} type='submit'>Sign up</Button>
+      </form>
+      <p>Don't have an account? <Link to="/login">Go Back</Link></p>
+    </div>
+  )
 }
